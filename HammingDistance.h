599df3f46c521d1e1a7d5651f460b6721aede56d
Laurenz Ruzicka <laurenz_ruzicka@gmx.at>
2022-06-06 22:28:14 +0200

    Restructured Program to allow for tests

--- HammingDistance.h
+++ HammingDistance.h
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-string int_to_str(const int input);
+string int_to_str(int input);
 int get_tetra_hamming_distance(string& aa, string& bb);
 
-int SEQUENCE_LENGTH, HAMMING_DISTANCE;
+inline int SEQUENCE_LENGTH, HAMMING_DISTANCE;
